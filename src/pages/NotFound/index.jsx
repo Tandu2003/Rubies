59ad6be058526e42e98877eef8c0b24bc1469d47
Tấn Dự <tandu542003@gmail.com>
2024-11-29 22:01:18 +0700

    feat add page not found

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,5 +1,28 @@
+import { Link } from "react-router-dom";
+import "./NotFound.scss";
+
+import Breadcrumb from "../../components/Breadcrumb";
+
+// import img
+import eror_404 from "../../assets/img/eror-404.png";
+
 const NotFound = () => {
-  return <></>;
+  return (
+    <>
+      <Breadcrumb breadcrumb="404 Không tìm thấy trang" />
+      <main className="not-found">
+        <div className="container">
+          <div className="box-not-found">
+            <img src={eror_404} alt="eror-404" />
+            <p>Chúng tôi không thể tìm thấy trang bạn yêu cầu.</p>
+            <Link to="/" className="btn-home">
+              Quay lại trang chủ
+            </Link>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default NotFound;
